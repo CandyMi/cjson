570e11a6ae11d72d64bd012389a31df16bbe30e7
869646063@qq.com <869646063@qq.com>
2022-07-19 20:44:29 +0800

    更新

--- json.h
+++ json.h
@@ -14,7 +14,30 @@
   #define xrio_free free
 #endif
 
-#define json_buffer_size (1024)
+/* Buffer 实现 */
+#define xrio_buffer_size (4096)
+
+typedef struct xrio_Buffer {
+  char* b; lua_State *L;
+  size_t bidx; size_t blen;
+  char ptr[xrio_buffer_size];
+} xrio_Buffer;
+
+#define xrio_buffgetidx(B)                (B->bidx)
+#define xrio_buffreset(B, idx)            ({B->bidx = idx;})
+
+#define xrio_pushliteral(B, s)            xrio_addlstring((B), (s), strlen(s))
+#define xrio_pushstring(B, s)             xrio_addlstring((B), (s), strlen(s))
+#define xrio_pushlstring(B, s, l)         xrio_addlstring((B), (s), l)
+#define xrio_pushfstring(B, fmt, ...)     ({xrio_addstring(B, lua_pushfstring(B->L, fmt, ##__VA_ARGS__)); lua_pop(L, 1);})
+
+void  xrio_buffinit(lua_State *L, xrio_Buffer *B);
+void* xrio_buffinitsize(lua_State *L, xrio_Buffer *B, size_t rsize);
+void  xrio_pushresult(xrio_Buffer *B);
+
+void  xrio_addchar(xrio_Buffer *B, char c);
+void  xrio_addstring(xrio_Buffer *B, const char *b);
+void  xrio_addlstring(xrio_Buffer *B, const char *b, size_t l);
 
 int json_cstring_to_utf8_hex(const char *hex);
 
